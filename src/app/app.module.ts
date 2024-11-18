7d68e26fb32ef5417d428661b2bedc781007d453
Ediier Andres Villaneda Navarro <ediervillaneda@gmail.com>
2024-11-17 19:13:27 -0500

    Implementacion componente animación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CountdownComponent } from './pages/countdown/countdown.component';
+import { AnimationComponent } from './pages/animation/animation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CountdownComponent
+    CountdownComponent,
+    AnimationComponent
   ],
   imports: [
     BrowserModule
